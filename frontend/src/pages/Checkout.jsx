ca5ed3cac4406a4eca19d13fd3c00b8d9d5effb4
SayaliSatam290204 <sayalisatam2925@gmail.com>
2026-08-17 16:06:57 +0530

    Fix syntax error in Checkout.jsx

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -363,6 +363,7 @@ const Checkout = () => {
                                                 </div>
                                             </div>
                                         </div>
+                                        );
                                     })}
                                 </div>
                                 
